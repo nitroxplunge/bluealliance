2242bb370b97223159ddbe7dd8d60b21d2b26e09
Isaac Wilcove <isaac@wilcove.com>
2017-12-26 16:54:48 -0800

    changed tests

--- examples/test.js
+++ examples/test.js
@@ -1,10 +1,6 @@
 var BlueAlliance = require("../");
 var tba = new BlueAlliance("53QztSBkXCtjaAgV98kdm6VyhgD0wQy30RReogRjxs8hPpsqDD6qmxFyz71WELeC");
 
-tba.getTeam(7308);
-tba.getTeamAwards(7308);
-tba.getTeamsAtEvent('casj', 2017);
-tba.getEventsForTeam(7308);
-tba.getMatchesAtEvent('casj', 2017);
-tba.getTeamsInMatch(tba.getMatchesAtEvent('casj', 2017)[0]);
-tba.isMatchDone(tba.getMatchesAtEvent('casj', 2017)[0]);
+tba.getTeam(7308).then(function(result) {
+    console.log(result);
+});
